72089959f2f74131caa2e588d767526241645e2c
Bernardo Andrade <berdfandrade@gmail.com>
2023-07-19 12:49:25 -0300

    --

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from "express";
 import gerarMonstro from "./monstro.js";
-import cors from 'cors'
+import cors from 'cors';
 const app = express();
 
-app.use(express());
-app.use(cors())
-
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send({"Mensagem" : "Use a rota /monstro, para gerar um monstro!"});
